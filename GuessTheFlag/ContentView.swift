844f097718779f116d9db49f878af1b09c8ea117
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2024-06-28 00:11:47 +0300

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Tuğçe Saygın on 27.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+    
+    @State private var counturies = ["Estonia", "Turkiye", "France" , "Germany" , "Ireland", "Italy" , "Nigeria" , "Poland" , "Spain" , "UK" , "Ukraine" , "US" ].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var scoreTotal = 0
+                        
+    var body: some View {
+        
+        ZStack{
+            
+            
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+          
+    
+            VStack(spacing: 30){
+                VStack{
+                
+                    Text("Tap the right flag")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(counturies[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                    
+                }
+                VStack(spacing: 30){
+                    ForEach(0..<3){ number in
+                        Button{
+                            flagTapped(number)
+                        }label :{
+                            Image(counturies[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 8)
+                        }
+                        
+                    }
+                }
+                
+                VStack{
+                    Text("🎯Your Score: \(scoreTotal)")
+                        .foregroundStyle(.white)
+                        .font(.title)
+                }
+                
+            }
+          
+            
+        }
+        
+        
+        .alert(scoreTitle, isPresented: $showingScore){
+            Button("Continue", action: askQuestion)
+        }
+        
+        
+    }
+    
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+            scoreTotal += 1
+            
+        }else{
+            scoreTitle = "Wrong"
+            scoreTotal = 0
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        counturies.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+  
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Tuğçe Saygın on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
