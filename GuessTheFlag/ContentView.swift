2984669b1af10d54a225d82adb65aa4c79a00bbf
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2024-06-29 20:11:37 +0300

    ui changes has made.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -22,48 +22,72 @@ struct ContentView: View {
         
         ZStack{
             
-            
-            LinearGradient(colors: [.teal, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops : [
+                .init(color: Color(red: 0.3 , green: 0.3, blue : 0.9), location: 0.3),
+                .init(color: Color(red: 0.4 , green: 0.6, blue : 0.8), location: 0.3)],
+                           center: .top, startRadius:200, endRadius: 700)
+            .ignoresSafeArea()
           
-    
-            VStack(spacing: 30){
-                VStack{
+          
+            VStack{
+               
+                Spacer()
                 
-                    Text("Tap the right flag")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
+                Text("Guess the flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                
+                VStack(spacing: 15){
                     
-                    Text(counturies[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                    
-                }
-                VStack(spacing: 30){
-                    ForEach(0..<3){ number in
-                        Button{
-                            flagTapped(number)
-                        }label :{
-                            Image(counturies[number])
-                                .clipShape(.capsule)
-                                .shadow(radius: 8)
-                        }
+                    VStack{
+                        
+                        Text("Tap the right flag")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(counturies[correctAnswer])
+                            .foregroundStyle(.black)
+                            .font(.largeTitle.weight(.semibold))
                         
                     }
+                    
+                    VStack(spacing: 30){
+                        ForEach(0..<3){ number in
+                            Button{
+                                flagTapped(number)
+                            }label :{
+                                Image(counturies[number])
+                                    .clipShape(.capsule)
+                                    .shadow(radius: 8)
+                            }
+                            
+                        }
+                    }
                 }
+               
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial.opacity(0.8))
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
                 
                 VStack{
                     Text("🎯Your Score: \(scoreTotal)")
                         .foregroundStyle(.white)
-                        .font(.title)
+                        .font(.title.bold())
                 }
+              
+                Spacer()
                 
             }
-            
-
+            .padding()
+           
             
         }
-        
+    
         
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
